110d327268040a6ceac3e869c1a5bba59cc12cad
ardhiansyah <ardhimetanaputra@gmail.com>
2018-03-16 09:41:07 +0700

    Add feature login

---
+++ controllers/user.controller.js
@@ -0,0 +1,37 @@
+const FB = require('fb');
+const User = require('../models/user.model');
+const token = require('../middlewares/token.middleware');
+
+module.exports = {
+    signin: (req, res) => {
+        if (!req.headers.access_token) res.status(404).send({ message: 'no access token' });
+        
+        FB.api('me', { fields: ['name', 'email'], access_token: req.headers.access_token }, (data) => {
+            if (data.error) return res.status(500).send({ message: data.error });
+
+            User.findOne({ email: data.email }, (err, user) => {
+                if (data.error) return res.status(500).send({ message: data.error });
+                
+                if (!user) {
+                    let new_user = new User({
+                        name: data.name,
+                        email: data.email,
+                    });
+                    console.log(new_user);
+                    new_user.save(err => {
+                        if (err) return res.status(500).send({ message: err });
+                        return res.status(201).send({
+                            message: 'signin success',
+                            token: token.generate({ id: new_user._id, name: new_user.name, email: new_user.email })
+                        });
+                    })
+                }
+
+                return res.status(200).send({
+                    message: 'signin success',
+                    token: token.generate({ id: user._id, name: user.name, email: user.email })
+                });
+            });
+        });
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const {createLeader,showLeader} = require('../controllers/leaderboard');
+const { signin } = require('../controllers/user.controller');
 
 router.post('/players-point', createLeader);
 router.get('/players-point', showLeader);
+router.post('/signin', signin);
 
 
 
